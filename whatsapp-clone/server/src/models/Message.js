0b234ad1212d28c2c416f107d833e2ee8cd25b44
Mallikharjuna Mulpuri <mulpm01@purdue.edu>
2026-02-17 18:19:22 -0500

    Added Server/authController, server/messageController

--- whatsapp-clone/server/src/models/Message.js
+++ whatsapp-clone/server/src/models/Message.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  messageType: { type: String, enum: ['text', 'image'], default: 'text' },
   status: { type: String, enum: ['sent', 'delivered', 'read'], default: 'sent' }
 }, { timestamps: true });
 
