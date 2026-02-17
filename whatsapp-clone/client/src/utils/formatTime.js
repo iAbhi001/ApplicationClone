ad135f72d71ea40e683e76035310289e808c4e18
Mallikharjuna Mulpuri <mulpm01@purdue.edu>
2026-02-17 18:14:31 -0500

    Added formatTime.js/client, index.css/client

---
+++ whatsapp-clone/client/src/utils/formatTime.js
@@ -0,0 +1,9 @@
+export const formatTime = (timestamp) => {
+  if (!timestamp) return "";
+  const date = new Date(timestamp);
+  return date.toLocaleTimeString([], { 
+    hour: '2-digit', 
+    minute: '2-digit',
+    hour12: true 
+  });
+};
